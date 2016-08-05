d7659dfcdf7bf388f7f2ba5c4c2c9b3e822af680
Lukas Taegert <lukas.taegert@tngtech.com>
2016-08-05 09:36:09 +0200

    Complete browserify example

--- pixi-browserify/src/main.js
+++ pixi-browserify/src/main.js
@@ -1,1 +1,8 @@
-console.log('Hallo2');
+const PIXI = require('pixi.js');
+
+const renderer = new PIXI.autoDetectRenderer();
+document.body.appendChild(renderer.view);
+
+const stage = new PIXI.Container();
+stage.addChild(new PIXI.Text('PIXI is working nicely', {fill: 0xffffff}));
+renderer.render(stage);
