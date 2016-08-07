42f89594d4a3ef6139d61e41af60824fb0f71bcd
Lukas Taegert <lukas.taegert@tngtech.com>
2016-08-07 16:37:22 +0200

    Simplify project structure example

--- 1-pixi-project-structure/main.js
+++ 1-pixi-project-structure/main.js
@@ -1,30 +1,24 @@
 const WIDTH = 700, HEIGHT = 800;
-
 const renderer = PIXI.autoDetectRenderer(WIDTH, HEIGHT);
-
 document.body.appendChild(renderer.view);
 
 const stage = new PIXI.Container();
 
-const circle = new PIXI.Graphics();
-circle.beginFill(0xff0000).drawCircle(0, 0, 100).endFill();
-circle.position.set(WIDTH / 2, HEIGHT / 2);
-stage.addChild(circle);
+const rect = new PIXI.Graphics()
+  .beginFill(0x0000ff)
+  .drawRect(0, 0, 100, 100)
+  .endFill();
+stage.addChild(rect);
+rect.position.set(WIDTH / 2 - 50, HEIGHT / 2 - 50);
 
-const rect = new PIXI.Graphics();
-rect.beginFill(0x0000ff).drawRect(0, 0, 50, 50).endFill();
-circle.addChild(rect);
-
-function updateStage(timeStamp) {
-  circle.position.set(WIDTH / 2 + Math.sin(timeStamp / 1000) * 50, HEIGHT / 2 + Math.cos(timeStamp / 1000) * 50);
-  circle.rotation = timeStamp / 1000;
-  circle.scale.set(2, 2);
+function updateStage() {
+  rect.rotation += 0.01;
 }
 
-function animationLoop(timeStamp = 0) {
-  updateStage(timeStamp);
+function performAnimationLoop() {
+  updateStage();
   renderer.render(stage);
-  window.requestAnimationFrame(animationLoop);
+  window.requestAnimationFrame(performAnimationLoop);
 }
 
-animationLoop();
+performAnimationLoop();
