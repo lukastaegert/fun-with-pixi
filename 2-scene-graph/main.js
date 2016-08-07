1ee89ba4f0013ed4b746bd8077031a040a4ff868
Lukas Taegert <lukas.taegert@tngtech.com>
2016-08-07 17:52:21 +0200

    Remove some code to prepare for the talk

--- 2-scene-graph/main.js
+++ 2-scene-graph/main.js
@@ -1,34 +1,20 @@
-const WIDTH = 700, HEIGHT = 800;
+const WIDTH = 600, HEIGHT = 800;
 const renderer = PIXI.autoDetectRenderer(WIDTH, HEIGHT);
 document.body.appendChild(renderer.view);
 
 const stage = new PIXI.Container();
 
-const rect = getRect(200, 0xff0000);
-function getRect(size, color) {
+function getCircle(centerX, centerY, size, color) {
   return new PIXI.Graphics()
-  .beginFill(color)
-    // in local coordinates
-  .drawRect(-size/2, -size/2, size, size)
-  .endFill();
+    .beginFill(color)
+    .drawCircle(centerX, centerY, size)
+    .endFill();
 }
-stage.addChild(rect);
-// in stage coordinates
-rect.position.set(WIDTH / 2, HEIGHT / 2);
 
-const innerRect = getRect(100, 0x00ff00);
-rect.addChild(innerRect);
-innerRect.position.x = 200;
-innerRect.rotation = 1;
+const circle = getCircle(0, 0, 200, 0xff0000);
+stage.addChild(circle);
 
-const text = new PIXI.Text('Hallo TNG', {fill: 0x0000ff});
-innerRect.addChild(text);
-text.scale.set(2, 4);
-text.resolution = 4;
-
-function updateStage() {
-  rect.rotation += 0.01;
-}
+performAnimationLoop();
 
 function performAnimationLoop() {
   updateStage();
@@ -36,4 +22,6 @@ function performAnimationLoop() {
   window.requestAnimationFrame(performAnimationLoop);
 }
 
-performAnimationLoop();
+function updateStage() {
+  circle.rotation += 0.01;
+}
--- 4-interactivity/main.js
+++ 4-interactivity/main.js
@@ -11,16 +11,5 @@ function setupStage() {
   const stage = new PIXI.Container();
   const sprite = PIXI.Sprite.fromFrame('0');
   stage.addChild(sprite);
-  sprite.interactive = true;
   renderer.render(stage);
-
-  sprite.on('mouseover', function() {
-    sprite.filters = [new PIXI.filters.InvertFilter()];
-    renderer.render(stage);
-  });
-
-  sprite.on('mouseout', function() {
-    sprite.filters = null;
-    renderer.render(stage);
-  });
 }
